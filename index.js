b210142fb71d46f98ec8aa458cde0d602b857fca
wellpinho <wellpinho@outlook.com>
2021-09-27 20:38:37 -0300

    write file with nodejs

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ fs.readFile('./upload/users/user.csv', { encoding: 'utf-8' }, (erro, data) => {
     console.log('Falha na leitura do arquivo:', erro);
   }
 });
+
+fs.writeFile('file-created.csv', 'CSV CRIADO!', (err) => {
+  console.log(err);
+})
