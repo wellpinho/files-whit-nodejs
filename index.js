f5e384ff71c3757315b65b2c8468a6a26d1a2cd6
wellpinho <wellpinho@outlook.com>
2021-09-27 19:56:32 -0300

    reading file with nodejs

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.readFile('./upload/users/user.csv', { encoding: 'utf-8' }, (erro, data) => {
+  try {
+    console.log(data)
+  } catch (error) {
+    console.log('Falha na leitura do arquivo:', erro);
+  }
+});
